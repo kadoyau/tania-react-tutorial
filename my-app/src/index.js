30a10f9e5b3af45a5d89a8a96b809710481b053a
kadoyau <el.psi.congroo@gmail.com>
2018-08-28 02:57:36 +0900

    もっと単純なところからはじめる
    CSSはprimitive CSS
    https://taniarascia.github.io/primitive/

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello, React!</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
