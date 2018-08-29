d5f0230fbe27a9c280b5677cd6f417f9c3ccd0db
kadoyau <el.psi.congroo@gmail.com>
2018-08-30 03:25:15 +0900

    Components:class componentsは再利用可能だけどこのままだとハードコード

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
+import Table from './Table';
 
 class App extends Component {
     render() {
-        const name = 'Mito';
-        const heading = <h1 className="site-heading">Hello, {name}</h1>;
         return (
-            <div className="App">
-                {heading}
+            <div className="container">
+                <Table/>
             </div>
         );
     }
