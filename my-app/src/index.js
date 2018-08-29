91eb158cc607a39d613f9c0df19c7ae8773b3845
kadoyau <el.psi.congroo@gmail.com>
2018-08-30 03:11:17 +0900

    JSXはJS。変数も使える

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,9 +5,11 @@ import './index.css';
 
 class App extends Component {
     render() {
+        const name = 'Mito';
+        const heading = <h1 className="site-heading">Hello, {name}</h1>;
         return (
             <div className="App">
-                <h1>Hello, React!</h1>
+                {heading}
             </div>
         );
     }
