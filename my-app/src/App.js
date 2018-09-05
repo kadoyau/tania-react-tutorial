e8aeebc5d1564d863f0876d8db3349c6b97d133c
kadoyau <el.psi.congroo@gmail.com>
2018-09-06 02:12:54 +0900

    フォームに入力した要素をテーブルに追加できるようになる

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,10 @@
 import React, {Component} from 'react';
 import Table from './Table';
+import Form from "./Form";
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                'name': 'Charlie',
-                'job': 'Janitor'
-            },
-            {
-                'name': 'Mac',
-                'job': 'Bounce'
-            },
-            {
-                'name': 'Dee',
-                'job': 'Aspiring actress'
-            },
-            {
-                'name': 'Dennis',
-                'job': 'Bartender'
-            }
-        ]
+        characters: []
     };
 
     removeCharacter = index => {
@@ -33,12 +17,19 @@ class App extends Component {
         })
     };
 
+    handleSubmit = character => {
+        this.setState({
+            characters: [...this.state.characters, character]
+        });
+    }
+
     render() {
         return (
             <div className="container">
                 <Table characterData={this.state.characters}
                        removeCharacter={this.removeCharacter}
                 />
+                <Form handleSubmit={this.handleSubmit}/>
             </div>
         );
     }
